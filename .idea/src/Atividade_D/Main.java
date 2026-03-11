b2cd1357b0e4ec856d1c54c7f9431a79fbcd3373
Luiz-Otavio15 <oluiz9790@gmail.com>
2026-03-10 23:44:51 -0300

    Main da atividade D

---
+++ .idea/src/Atividade_D/Main.java
@@ -0,0 +1,15 @@
+package Atividade_D;
+
+public class Main {
+    static void main(String[] args) {
+
+        System.out.println("Executando TesteVetor7...");
+        TesteVetor7.main(args);
+
+        System.out.println("\n---------------------------\n");
+
+        System.out.println("Executando TesteVetor8...");
+        TesteVetor8.main(args);
+
+    }
+}
